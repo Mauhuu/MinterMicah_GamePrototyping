1873e94d40478485320e47bd1fbed15c4fa96498
Mauhuu <38054962+Mauhuu@users.noreply.github.com>
2018-05-08 19:43:52 -0400

    Almost works
    The charging is set to false after timer is done, but doesn't fire.

---
+++ Assets/Countdown.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+    public GameObject CannonPlayer;
+    public int timeLeft = 5;
+    public Text countdownText;
+
+    // Use this for initialization
+    void Start()
+    {
+       
+        StartCoroutine("LoseTime");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        countdownText.text = ("Time Left = " + timeLeft);
+
+        if (timeLeft <= 0)
+        {
+            CannonPlayer.GetComponent<PressSpace>().IsCharging = false;
+            StopCoroutine("Losetime");
+            countdownText.text = "Times Up!";
+
+        }
+
+    }
+
+    IEnumerator LoseTime()
+    {
+
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+
+
+
+        }
+    }
+}
--- Assets/PressSpace.cs
+++ Assets/PressSpace.cs
@@ -4,47 +4,45 @@ using UnityEngine;
 
 public class PressSpace : MonoBehaviour {
 
-    public GameObject Boulder_Emitter;
-
-    public GameObject Boulder1;
-
     public float Forward_Force = 0;
 
-	Rigidbody2D _rigid;
+    Rigidbody2D _rigid;
 
-	public bool IsCharging = true;
+    public bool IsCharging = true;
 
-	// Use this for initialization
-	void Start () {
-		_rigid = GetComponent<Rigidbody2D> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start() {
+        _rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
 
 
-		//if (TimeElapsed > LaunchWindow) {
-		//	IsCharging = false;
-		//}
+        //if (TimeElapsed > LaunchWindow) {
+        //	IsCharging = false;
+        //}
 
-		if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-			if (IsCharging) {
-				Charge ();
-			} else {
-				Fire ();
-			}
-				        }
-		
-	}
+            if (IsCharging) {
+                Charge();
+            } else {
+                Fire();
+            }
+        }
 
-	void Fire()
-	{
-		_rigid.bodyType = RigidbodyType2D.Dynamic;
-		_rigid.AddForce (transform.right * Forward_Force, ForceMode2D.Impulse);
-	}
+    }
 
-	void Charge(){
+    void Fire()
+    {
+        _rigid.bodyType = RigidbodyType2D.Dynamic;
+        _rigid.AddForce(transform.right * Forward_Force, ForceMode2D.Impulse);
+    }
+	
+
+	void Charge()
+    {
 
 		Forward_Force = Forward_Force + 5;
 
